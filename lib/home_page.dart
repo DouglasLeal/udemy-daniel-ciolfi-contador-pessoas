a5e98642e08586052851802a1d6ae4f84b0f5270
Douglas Leal <douglas.leal@live.com>
2023-02-03 20:45:59 -0300

    Botões criados e funções para aumentar e diminuir número de pessoas criadas

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+
+  const HomePage({ Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int pessoas = 0;
+
+  aumentarPessoas(){
+    setState(() {
+      pessoas++;
+    });
+  }
+
+  diminuirPessoas(){
+    setState(() {
+      pessoas--;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,14 +30,33 @@ class HomePage extends StatelessWidget {
       body: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
+          children: [
+            const Text(
               "Pode Entrar!",
-              style: TextStyle(fontSize: 32, color: Colors.white,),
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.white,
+              ),
             ),
             Text(
-              "0",
-              style: TextStyle(fontSize: 100, color: Colors.white,),
+              pessoas.toString(),
+              style: const TextStyle(
+                fontSize: 100,
+                color: Colors.white,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: diminuirPessoas,
+                  child: const Text("-1"),
+                ),
+                TextButton(
+                  onPressed: aumentarPessoas,
+                  child: const Text("+1"),
+                ),
+              ],
             ),
           ],
         ),
